ac185b55854c728ecfcce43a8296634cf8128494
CAOSSLL <caossll89@gmail.com>
2014-05-18 14:50:24 +0200

    Tablero

--- public_html/recursos/js/main.js
+++ public_html/recursos/js/main.js
@@ -1,32 +1,42 @@
 var host = "http://localhost/";
 var server = "MonopolyServer/web/app_dev.php/";
 var app = "MonopolyClient/public_html/";
+var modulo = "";
 
-var contenido ="";
+var contenido = "";
 
-$(document).ready(function(){
-    
-    contenido = $("#contenido");
-    
-    cargarInicio();
-    
+$(document).ready(function() {
+
+    contenido = $("#contenido"); 
+    modulo = "inicio";
+
+
+    cargarModulo();
+
     $.ajax({
         url: host + server + "usuario/autenticado",
         method: "post",
         dataType: "json",
-        success: function(datos){
-            if(datos.autenticado){
+        success: function(datos) {
+            if (datos.autenticado) {
                 $("#usuario").text("Hola amo");
-            }else{
+            } else {
                 $("#usuario").text("Logueate");
             }
         }
     });
-    
-    
-    
-});
 
-function cargarInicio(){
-    contenido.load(host + app + "inicio.html");
-}
+    function cargarModulo() {
+        contenido.hide("bind");
+        contenido.load(host + app + modulo + ".html", function() {
+            cargarEventos();
+            $(".ir").off().on("click", function(e) {
+                e.preventDefault();
+                modulo = $(this).attr("href");
+                cargarModulo();
+            });
+            contenido.show("bind");
+        });
+    }
+
+});
