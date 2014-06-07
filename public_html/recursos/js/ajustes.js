3abf13d66168b6517556fa4a8ebd80908e0410f0
Olga Juste <olgajuste.92@gmail.com>
2014-06-07 10:41:38 +0200

    Ajustes

--- public_html/recursos/js/ajustes.js
+++ public_html/recursos/js/ajustes.js
@@ -1,6 +1,44 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+$(document).ready(function() {
 
+    if (logado()) {
 
+        $("#formCambiarUsuario").submit(function(e) {
+            e.preventDefault();
+            var errores = 0;
+            if (validaciones()) {
+                alert("bien");
+            } else {
+                alert("mal");                
+            }
+        });
+    }
+    ;
+});
+
+function logado() {
+    if (usuario["nombre"] === "") {
+        modulo = "inicio";
+        cargarModulo(modulo);
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function validaciones() {
+    // Recogemos todos los inputs que vamos a validar y creamos un contador de errores
+    var inputNuevoUsuario = $("input[name=nuevoUsuario]");
+    var contErrores = 0;
+
+    // Comprabamos si los campos son válidos
+    if (!validarUsuario(inputNuevoUsuario))
+        contErrores++;
+
+    if (contErrores === 0) {
+//        $("#error-registro").text("");
+        return true;
+    } else {
+//        $("#error-registro").text("Corrija los errores, por favor");
+        return false;
+    }
+}
