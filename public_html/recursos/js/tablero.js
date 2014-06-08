b6c4c1c3090d76c2146b2aecd9ba493663654c23
Caos SLL <caos.sll.89@gmail.com>
2014-06-08 10:59:16 +0200

    Modificación de todos desde Github de todo

--- public_html/recursos/js/tablero.js
+++ public_html/recursos/js/tablero.js
@@ -2,6 +2,7 @@ var clikado = false;
 var posicionesCasilla = new Array();
 var turno = 0;
 var tieneTurno = false;
+var bote = 0;
 
 $(document).ready(function() {
 
@@ -9,6 +10,11 @@ $(document).ready(function() {
 //    iniciarDado();
     iniciarFichas();
     cargarDatosJugadores();
+    $("#botonComprar").off().on("click", function(e) {
+        comprar();
+    });
+    
+    $("#paneles").accordion();
 
 });
 
@@ -19,6 +25,22 @@ socket.on("movimiento_partida", function(datos) {
     cambiarTurno();
 //    turno = datos.turno;
 });
+
+socket.on("cambio_bote", function(datos) {
+    trace("Evento cambiar bote-->");
+    $("#bote").text(datos.bote);
+    bote = datos.bote;
+});
+
+socket.on("infoPartida", function(datos) {
+//    trace("Evento cambiar bote-->");
+    $("#infoPartida").text(datos.texto);
+});
+
+function emitirInformacion(texto) {
+    $("#infoPartida").text(texto);
+    socket.emit("infoPartida", {sala: sala, texto: texto});
+}
 
 function iniciarDado() {
     trace("Iniciar dado -->");
@@ -38,11 +60,33 @@ function iniciarFichas() {
 
 function cargarDatosJugadores() {
     trace("Cargar datos jugadores -->");
-    // Cargar fichas
+    var panel = $(".panel");
+    var cabecera = $(".cabecera");
+    var jugadorCab, jugadorPan;
+    // Cargar fichas y paneles información jugadores
     for (var i = 0; i < listaUsuarios.length; i++) {
         console.log(listaUsuarios[i]);
         $("#tablero").append("<div class='ficha' id='" + listaUsuarios[i].personajeNombre + "'><img src='recursos/images/fichas/" + listaUsuarios[i].personajeNombre + ".png'></div>");
         posicionesCasilla.push(0);
+
+        if (i === 0) {
+            jugadorCab = cabecera;
+            jugadorPan = panel;
+            
+        } else {
+            jugadorCab = cabecera.clone();
+            jugadorPan = panel.clone();
+            $("#paneles").append(jugadorCab);
+            $("#paneles").append(jugadorPan);
+            jugadorCab.attr("id", "cabUsu" + i);
+            jugadorPan.attr("id", "panelUsu"+i);
+        }
+        
+        jugadorCab.find(".nombre").text(listaUsuarios[i].nombre);
+        jugadorCab.find(".personaje").text(listaUsuarios[i].personajeNombre);
+        jugadorCab.find(".dinero").text(listaUsuarios[i].dinero);
+        jugadorPan.find(".tarjetas").text("Pruebas");
+        
     }
 
     if (usuario.id == listaUsuarios[turno].id) {
@@ -60,10 +104,11 @@ function cargarDatosJugadores() {
 
 function tirar() {
     trace("Tirar dados -->");
-    var num = Math.floor(Math.random() * (6 - 1 + 1)) + 1
+    var num = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
     $(".dado").text(num);
     moverFicha(num);
-    socket.emit("movimiento_partida", { sala: sala, avance: num, turno: turno});
+    socket.emit("movimiento_partida", {sala: sala, avance: num, turno: turno});
+    $("#botonComprar").removeAttr("disabled");
     cambiarTurno();
 }
 
@@ -76,19 +121,23 @@ function cambiarTurno() {
     }
 
     if (usuario.id == listaUsuarios[turno].id) {
-        tieneTurno = true;
-        $(".dado").css({backgroundColor: "#f2ea9d"});
-        $(".dado").bind("click", tirar);
+        if (usuario.carcel === 0) {
+            tieneTurno = true;
+            $(".dado").css({backgroundColor: "#f2ea9d"});
+            $(".dado").bind("click", tirar);
+        } else {
+            usuario.carcel -= 1;
+        }
     } else {
         tieneTurno = false;
         $(".dado").css({backgroundColor: "gray"});
         $(".dado").unbind("click");
     }
 
-    console.log("turno: " + turno + " tiene turno? " + tieneTurno);
+    $("#infoPartida").text("El turno es de: " + listaUsuarios[turno].nombre);
 }
 
-function moverFicha(avance) {
+function moverFicha(avance, noCobrar) {
     trace("Mover ficha -->");
     var personaje = listaUsuarios[turno].personajeNombre;
     var ficha = $("#" + personaje);
@@ -107,7 +156,7 @@ function moverFicha(avance) {
 
 function iniciarTablero() {
     trace("Iniciar tablero -->");
-    
+
     var tablero = $("#tablero"); //Obtener el tablero como objeto JQuery
 
     var tabla;
@@ -163,6 +212,7 @@ function iniciarTablero() {
 
     tablero.html(tabla);
 
+    zoom();
 }
 
 function zoom() {
@@ -217,10 +267,133 @@ function zoom() {
     });
 }
 
-function trace(mensaje){
-    try{
+function trace(mensaje) {
+    try {
         console.log(mensaje);
-    }catch (e){
-        
+    } catch (e) {
+
     }
+}
+
+function tienePosesiones() {
+    $.ajax({
+        url: host + server + "posesioncasilla/comprobar/" + usuario.id + "/" + idPartida,
+        method: "post",
+        dataType: "json",
+        success: function(datos) {
+            if (!isNaN(datos.idJugador)) {
+                $("#botonHipotecar").removeAttr("disabled");
+            }
+        },
+        error: function(e) {
+            console.log("error");
+        }
+    });
+}
+
+function comprobarTipoCasilla(idCasilla) {
+    $.ajax({
+        url: host + server + "casilla/" + idCasilla + "/show",
+        method: "post",
+        dataType: "json",
+        success: function(datos) {
+            switch (datos.tipo) {
+                case "especial":
+                    casillasEspecial(datos);
+                    break;
+                case "tierra", "tarjetas", "bastones", "caballos":
+                    comprobarPosesionCasilla(idCasilla);
+                    break;
+                case "impuestos":
+                    casillasImpuestos(datos);
+                    break;
+                default :
+                    alert("No hay tipo de casilla O.O");
+                    break;
+            }
+        },
+        error: function(e) {
+            console.log("error");
+        }
+    });
+}
+
+function casillasEspecial(datos) {
+    switch (datos.numero) {
+        case 20:
+            usuario.dinero += parseInt(bote);
+            bote = 0;
+            $("#bote").text(bote);
+            socket.emit("cambio_bote", {sala: sala, bote: bote});
+            break;
+        case 30:
+            usuario.carcel = 3;
+            moverFicha(20);
+            break;
+    }
+}
+
+function casillasImpuesto(datos) {
+    usuario.dinero -= paseInt(datos.precio);
+    bote += paseInt(datos.precio);
+    $("#bote").text(bote);
+    socket.emit("cambio_bote", {sala: sala, bote: bote});
+}
+
+function comprobarPosesionCasilla(idCasilla) {
+    $.ajax({
+        url: host + server + "posesioncasilla/comprobarPosesion/" + idCasilla + "/" + idPartida,
+        method: "post",
+        dataType: "json",
+        success: function(datos) {
+            if (isNaN(datos.idJugador.id)) {
+                if (usuario.dinero >= datos.idCasilla.precio) {
+                    $("#botonComprar").removeAttr("disabled");
+                }
+            } else if (datos.idJugador.id !== usuario.id) {
+                alert("te toca pagar");
+            } else {
+                alert("es tuya");
+            }
+        },
+        error: function(e) {
+            console.log("error");
+        }
+    });
+}
+
+function comprar() {
+    //Obtener casilla
+    $.ajax({
+        url: host + server + "casilla/" + idCasilla + "/show",
+        method: "post",
+        dataType: "json",
+        success: function(datosCasilla) {
+            //Modificamos bd, añadimos registro de PosesionCasilla y Jugador
+            $.ajax({
+                url: host + server + "posesioncasilla/comprar/" + idCasilla + "/" + usuario.id + "/" + idPartida + "/" + usuario.dinero,
+                method: "post",
+                dataType: "json",
+                success: function(datosCrear) {
+                    if (datosCrear.idPosesionCasilla !== 0) {
+                        //Restamos al jugador
+                        usuario.dinero -= datosCasilla.precio;
+                        var texto = "El usuario " + usuario.nombre + " ha comprado la casilla " + datosCasilla.nombre;
+                        emitirInformacion(texto);
+                        //Añadir carta a panel
+                    } else {
+                        alert("No se ha podido realizar la compra");
+                        var texto = "El usuario " + usuario.nombre + " no ha podido comprar la casilla " + datosCasilla.nombre;
+                        emitirInformacion(texto);
+                    }
+                },
+                error: function(e) {
+                    console.log("error");
+                }
+            });
+        },
+        error: function(e) {
+            console.log("error");
+        }
+    });
 }
