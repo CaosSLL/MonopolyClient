d71d4d8add536eeacd09b98338d700870dcd1a52
Caos SLL <caos.sll.89@gmail.com>
2014-06-08 10:57:47 +0200

    Modificación de todos desde Github de todo

--- public_html/recursos/js/inicio.js
+++ public_html/recursos/js/inicio.js
@@ -1,59 +1,71 @@
 $(document).ready(function() {
 
     //Enlace para ir a registro
-    
-    $(".ir").off().on("click", function(e) {
+
+    $(".irFormulario").off().on("click", function(e) {
         e.preventDefault();
-        if(modulo != $(this).attr("href")) {
+        if (modulo != $(this).attr("href")) {
             modulo = $(this).attr("href");
             cargarModulo(modulo);
         }
-    }); 
-    
+    });
+
     /*Método para gestionar el boton*/
-    
+
     $("#formLogueo").submit(function(e) {
-        
-        var errores = 0;
-        
-        e.preventDefault();        
-//        if(validarNombre($("#usuarioLogueo"))){
-//            USUARIO = $("#usuarioLogueo").val();
-//            alert("usuario correcto" + USUARIO);
-//        }else{
-//            errores = 1;
-//            alert("El usuario no es correcto");
-//        }
-//        if(validarContrasenia($("#contraseniaLogueo"))){
-//            CONTRASENIA = $("#contraseniaLogueo").val();            
-//        }else{
-//            errores = 1;
-//        }
-        
-        if(errores === 0){
+
+        var usuarioValidado = false;
+        var passwordValidada = false;
+
+        e.preventDefault();
+
+        if (!validarUsuario($("#usuarioLogueo"))) {
+            usuarioValidado = false;
+        }else{
+            usuarioValidado = true;
+        }
+        if (!validarPassword($("#contraseniaLogueo"))) {
+            passwordValidada = false;
+        }else{
+            passwordValidada = true;
+        }
+
+        if (usuarioValidado && passwordValidada) {
             $.ajax({
-                url: host + server + "usuario/login", 
-                method : "post", 
-                dataType : "json",
-                data : $(this).serialize(),
-                success : function(datos){
+                url: host + server + "usuario/login",
+                method: "post",
+                dataType: "json",
+                data: $(this).serialize(),
+                success: function(datos) {
                     if (datos.autenticado) {
                         $("#usuario").text(datos.nombre);
                         usuario.id = datos.id;
                         usuario.nombre = datos.nombre;
                         modulo = "partidas";
                         cargarModulo(modulo);
+                        $(".error").text("");
                     } else {
                         alert(datos.error);
                         $("#usuario").text("Logueate");
+                        $(".error").text("El usuario o contraseña no es correcto");
                     }
                 },
-                error : function(e){
+                error: function(e) {
                     console.log("error");
                 }
             });
+        } else {
+            if (!usuarioValidado && !passwordValidada) {
+                $(".error").text("El usuario y la contraseña tiene una estructura incorrecta");
+            }else if (!usuarioValidado){
+                $(".error").text("El usuario tiene una estructura incorrecta");
+            }else if(!passwordValidada){
+                $(".error").text("La contraseña tiene una estructura incorrecta");
+            }else{
+                $(".error").text("Error desconocido");                
+            }
         }
-        
+
     });
 
 });
